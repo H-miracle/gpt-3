556d7af52c8c7c704a641c2d6ed9e899ce74b25c
H-miracle <deboimiracle@gmail.com>
2022-10-18 20:50:34 +0000

    project completed

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Denu Volta P.O. box 127 Denu, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Accra, Ghana</p>
+        <p>+233-54-397-6961</p>
+        <p>miraclefx3@gmail.com</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2022 Miracle Fx. All rights reserved.</p>
+    </div>
+  </div>
   )
 }
 
